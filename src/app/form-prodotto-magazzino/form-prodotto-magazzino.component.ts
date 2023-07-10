63ec39aa5c8368b17421f4408c497aea840f0418
LukeWarmWater32 <137297263+LukeWarmWater32@users.noreply.github.com>
2023-07-10 12:17:21 +0200

    ProdottoMag

--- src/app/form-prodotto-magazzino/form-prodotto-magazzino.component.ts
+++ src/app/form-prodotto-magazzino/form-prodotto-magazzino.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProdottoMagazzino } from "../model/prodotto-magazzino";
+import { ProdottoCrudService } from "../servizi/prodotto-crud.service";
 
 @Component({
-  selector: 'app-form-prodotto-magazzino',
-  templateUrl: './form-prodotto-magazzino.component.html',
-  styleUrls: ['./form-prodotto-magazzino.component.css']
+  selector: "app-form-prodotto-magazzino",
+  templateUrl: "./form-prodotto-magazzino.component.html",
+  styleUrls: ["./form-prodotto-magazzino.component.css"],
 })
-export class FormProdottoMagazzinoComponent {
+export class FormProdottoMagazzinoComponent implements OnInit {
+  prodottoMagazzino: ProdottoMagazzino = new ProdottoMagazzino();
+  message!: string;
 
+  constructor(private crud: ProdottoCrudService) {}
+
+  ngOnInit(): void {}
+
+  addIntoMagazzino(prodMag: ProdottoMagazzino): void {
+    this.crud.addProdottoMagazzino(prodMag).subscribe((message: string) => {
+      this.message = message;
+    });
+  }
+
+  resetMessage(): void{
+    this.message = "";
+  }
 }
